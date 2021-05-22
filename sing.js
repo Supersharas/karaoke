d949ebeb057c5a34d29fd9bb0b14ee7b73994258
jhon <supersharas@gmail.com.com>
2021-05-22 11:45:28 +0100

    some experiments done

--- sing.js
+++ sing.js
@@ -1,3 +1,19 @@
 
+var lines = document.getElementById('song').children;
+var l = lines.length;
+console.log('lines', lines);
 
-var lines = document.get
+
+
+function singing(lineNo){
+	lines[lineNo].classList.add('sing');
+	console.log('lineNo, l', lineNo, l);
+	console.log('singing', lines[lineNo]);
+	if (lineNo + 1 < l){
+		setTimeout(function(){
+			singing(lineNo + 1)
+		}, 5000);
+	}
+}
+
+singing(0);
